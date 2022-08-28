622c8eee89939ff4f58a244ce44134df7a9081de
Artem <rrampage1991@gmail.com>
2022-08-28 17:50:41 +0300

    second commit

---
+++ Task_01/Program.cs
@@ -0,0 +1,34 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами.
+
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().NextDouble();
+        }
+    }
+}
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите высоту массива = ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите длинну массива = ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+double[,] matrix = new double[m, n];
+
+FillArray(matrix);
+PrintArray(matrix);
